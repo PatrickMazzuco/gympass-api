16aaf79c87d105259182cb9c8f26dcd2fd164597
PatrickMazzuco <patrickmazzuco@gmail.com>
2023-05-02 16:07:04 -0300

    chore: add vite ui and exclude error files from coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -6,5 +6,12 @@ export default defineConfig({
   test: {
     globals: true,
     clearMocks: true,
+    coverage: {
+      exclude: [
+        ...configDefaults.exclude,
+        'src/tests/**/*',
+        'src/**/*.error.ts',
+      ],
+    },
   },
 });
